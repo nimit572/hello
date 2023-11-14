193525a87155731142fac2162250f0407255a3c9
nimit572 <nimitkapoor17@gmail.com>
2023-11-14 12:38:48 +0530

    updated

--- app.py
+++ app.py
@@ -7,4 +7,13 @@ def hello():
      return 'Hello, World! This is my first Flask app.' 
  
 if __name__ == '__main__': 
-     app.run(debug=True, host='0.0.0.0', port=80) 
+     app.run(debug=True, host='0.0.0.0', port=80)
+
+# After Update
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, Updated World!'
